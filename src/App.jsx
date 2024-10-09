55a7e0518250a379bba2dc2ce244b4a40811a638
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-10-09 16:26:40 +0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import "./index.css";
-import { Container, Section } from "./components";
+import { Container, Section, ArticleList, SearchForm } from "./components";
+
+import { fetchArticlesWithTopic } from "./services/api";
 
 const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async (topic) => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      console.log(data);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Section>
-      <Container></Container>
+      <Container>
+        <div>
+          <SearchForm onSearch={handleSearch} />
+          {loading && <p>Loading</p>}
+          {error && <Error />}
+          {articles.length > 0 && <ArticleList items={articles} />}
+        </div>
+      </Container>
     </Section>
   );
 };
